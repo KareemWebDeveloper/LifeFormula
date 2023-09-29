b43fcc004789a4ec87c848841606642838ba583e
KareemWebDeveloper <kemoayman2015@gmail.com>
2023-09-29 17:52:26 +0300

    deploying dashboard

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,11 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  build: {
+    rollupOptions: {
+      external: ['primevue/config'],
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
