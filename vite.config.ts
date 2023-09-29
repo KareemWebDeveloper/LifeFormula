804b7155d4cc597b0d5ee34fc684fa5ddc9a9cd3
KareemWebDeveloper <kemoayman2015@gmail.com>
2023-09-29 18:01:35 +0300

    deploting the project

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,6 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  build: {
-    rollupOptions: {
-      external: ['primevue/config' , 'primevue/resources/themes/lara-light-indigo/theme.css' , 'primevue/resources/primevue.css'],
-    },
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
